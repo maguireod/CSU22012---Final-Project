9cb455ac7d7d96a61174a264ec5b15bdfe247ef6
omaguire <omaguire@Lenovo-YogaL13.corasystems.local>
2022-04-06 00:24:04 +0100

    Added functionality into ShortestPath class

--- FinalProjectCSU22012/ShortestPath.java
+++ FinalProjectCSU22012/ShortestPath.java
@@ -1,16 +1,28 @@
-
+import java.util.Scanner;
+import java.io.*;
 public class ShortestPath {
-	
-	public ShortestPath() {
-		
+	public static int Stops;
+	public static double[][] tripCosts;
+	public static void highestStopNumber() {
+		int maximum = 0;
+		try {
+			File file = new File("stops.txt");
+		    Scanner f = new Scanner(file);
+		    Scanner s;
+		    int i = 0;
+		    int count = 0;
+		    while(f.hasNextLine()) {
+		    	s = new Scanner(f.nextLine());
+		    	s.useDelimiter(",");
+		    	if(count != 0) {
+		    	    if((i = s.nextInt()) > maximum) 
+		    	    	maximum = i;
+		    	}
+		    	count++;
+		    }
+		    f.close();
+		    Stops = maximum + 1;
+		    tripCosts = new double[Stops][Stops];
+		} catch(Exception e) {}
 	}
-	//use Dijkstra's implementation
-	public String findShortestPath(int source, int sink) {
-		return "Hello";
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
