21107d097ca2a848212fe533779abebee4e88cb8
omaguire <omaguire@Lenovo-YogaL13.corasystems.local>
2022-04-08 17:34:08 +0100

    Created new Class to implement the TST

--- FinalProjectCSU22012/SearchBusStop.java
+++ FinalProjectCSU22012/SearchBusStop.java
@@ -1,10 +1,16 @@
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class SearchBusStop {
 	
 	// Implements TST Alogorithm
-	public static void takeUserInput() {}
-	public static void implementSearch() {};
-	
+	public static void takeUserInput() {
+		JPanel screen = new JPanel();
+		screen.add(new JLabel("Choose a Bus Stop Search Option:"));
+		Object[] instructions = {"Search by Full Name", "Search by First Few Characters "};
+		int option = JOptionPane.showOptionDialog(null, screen, "Bus Management System", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, instructions, null);
+	}
 	public static void main(String[] args) {
 	
 
