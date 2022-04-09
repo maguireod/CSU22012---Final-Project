55f0ea4b7ae99f203ae26039f21708af7dac41f9
omaguire <omaguire@Lenovo-YogaL13.corasystems.local>
2022-04-09 15:57:20 +0100

    Adding functionality to the SearchBusStop class - to interact with TST.java - Decently finished, potential errors that need to be addressed

--- FinalProjectCSU22012/SearchBusStop.java
+++ FinalProjectCSU22012/SearchBusStop.java
@@ -1,6 +1,6 @@
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import java.io.*;
+import java.util.Scanner;
+import javax.swing.*;
 
 public class SearchBusStop {
 	
@@ -10,10 +10,60 @@ public class SearchBusStop {
 		screen.add(new JLabel("Choose a Bus Stop Search Option:"));
 		Object[] instructions = {"Search by Full Name", "Search by First Few Characters "};
 		int option = JOptionPane.showOptionDialog(null, screen, "Bus Management System", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, instructions, null);
-	}
-	public static void main(String[] args) {
-	
-
-	}
+	    screen = new JPanel();
+	    screen.add(new JLabel("Enter Bus Stop Name: "));
+	    Object[] submit = {"Enter"};
+	    JTextField input = new JTextField(20);
+	    screen.add(input);
+	    int clicked = JOptionPane.showOptionDialog(null, screen, "Bus Management System", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, submit, null);
+	    if(clicked == 0) {
+	    	TST<String> tst = new TST<String>();
+	    	CreateTST("stops.txt", tst);
+	    	findUsingTST(option, (input.getText().toUpperCase()), tst);
+	    }
+	}	
+	public static void CreateTST(String filename, TST<String> st) {
+    	try {
+    		Scanner file = new Scanner(new File(filename));
+    		String[] values;
+    		while(file.hasNextLine()) {
+    			values = file.nextLine().trim().split(",");
+    			String lineInfo = " | Stop Id: " + values[0] + " | Stop Code: " + values[1] + " | Stop Desc: " + values[3] + 
+    					" | Stop Lat: " + values[4] + " | Stop Lon: " + values[5] + " | Zone Id: " + values[6];
+    		    st.put(move(values[2]), lineInfo);
+    		}
+    	} catch (Exception e) {
+    		JOptionPane.showMessageDialog(null, "Error: System requires maintenance.");
+    	}
+    }
+    public static String move(String stop) {
+    	if(Character.isWhitespace(stop.charAt(2))) {
+    		int extend = stop.length() + 3;
+    		char[] arrayExtended = new char[extend];
+    		for(int i = 0; i < stop.length(); i ++) 
+    			arrayExtended[i] = stop.charAt(i);
+    		arrayExtended[extend -3] = ' ';
+    		arrayExtended[extend - 2] = arrayExtended[0];
+    		arrayExtended[extend - 1] = arrayExtended[1];
+    		return String.valueOf(arrayExtended).substring(3);
+    	}
+    	return stop;
+    }
+    public static void findUsingTST(int type, String input, TST<String> tst) {
+    	String stops = "";
+    	if(type == 1) {
+    		for(String newStop: tst.keysWithPrefix(input)) 
+    			stops = stops + newStop + tst.get(newStop) + "\n";
+    		if(stops == "")
+    			stops = "No Bus Stops Found";
+    	}
+    	else {
+    		if(tst.get(input) != null)
+    			stops = input + tst.get(input);
+    		else
+    			stops = "No Bus Stop Found.";
+    	}
+    	JOptionPane.showMessageDialog(null, stops);
+    }
 
 }
